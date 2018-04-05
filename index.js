ee736a39f96948db721c1a4314b3f6c3816b9c5a
Daniel Doughty <djdoughty@gmail.com>
2018-04-05 19:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 1) {
+  if (cardNumber == null) {
     console.log ('Sorry, we don\'t have a credit card on file for you')
   }
 }
